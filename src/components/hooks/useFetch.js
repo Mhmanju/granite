53e4ceb4e14ff98580d25f705da091984c849164
utthamh <utthamcsc@gmail.com>
2020-12-06 15:57:37 +0530

    host

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -5,7 +5,7 @@ export const useFetch=(url)=>{
 const [data,setData]=useState(null);
 const [loading,setLoading]=useState(false);
 const [error,setError]=useState(null);
-const baseurl='http://mhmanju.github.io/granite/'
+const baseurl='http://mhmanju.github.io/granite/data/'
 useEffect(async()=>{
     try{
         setLoading(true)
